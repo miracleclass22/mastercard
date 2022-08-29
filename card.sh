ff1fd10564515e926d3b1fd039688f91bdbf4ce0
miracleclass22 <miracleclass22@gmail.com>
2022-08-29 00:13:46 +0000

    our modify commits 3

--- card.sh
+++ card.sh
@@ -1,4 +1,20 @@
-card number
+ i-03fbb6d3d4ba2b022 (RedHad8-Demo-server)
+Public IPv4 address
+ 34.239.105.16 | open address 
+Private IPv4 addresses
+ 172.31.84.185
+IPv6 address
+–
+Instance state
+ Running
+Public IPv4 DNS
+ ec2-34-239-105-16.compute-1.amazonaws.com | open address 
+Hostname type
+IP name: ip-172-31-84-185.ec2.internal
+Private IP DNS name (IPv4 only)
+ ip-172-31-84-185.ec2.internal
+Answer private resource DNS name
+–card number
 card color
 card types
 car name
